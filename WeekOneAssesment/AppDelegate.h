22b985356cbb0fadb4041e27d4d6ea51a5986e5c
irwingonzales92 <irwin@newdesto.com>
2015-01-09 09:13:26 -0800

    Initial Commit

---
+++ WeekOneAssesment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeekOneAssesment
+//
+//  Created by Irwin Gonzales on 1/9/15.
+//  Copyright (c) 2015 Irwin Gonzales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeekOneAssesment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeekOneAssesment
+//
+//  Created by Irwin Gonzales on 1/9/15.
+//  Copyright (c) 2015 Irwin Gonzales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
